95726892ef943bb266f82fc819e70271aac18696
Rafael G. Mantovani <rgmantovani@gmail.com>
2016-11-09 15:17:15 -0200

    adding batchtools config file

---
+++ batchtools.conf.R
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+
+# Change to the number of cores in the machine - remove the parameter from the function
+# cluster.functions = makeClusterFunctionsMulticore(ncpus = 2)
+cluster.functions = makeClusterFunctionsInteractive()
+batchtools.verbose = TRUE
+
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
